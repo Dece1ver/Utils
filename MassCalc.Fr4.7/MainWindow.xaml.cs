860af6373bf7ed995294fb96365cc7aa1c6409ca
Dece1ver <dece1ver@pm.me>
2021-07-21 15:06:35 +0300

    Калькулятор массы круга

---
+++ MassCalc.Fr4.7/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using static MassCalc.Fr4._7.Util;
+
+namespace MassCalc.Fr4._7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string densityString;
+        private string diameterString;
+        private string lengthString;
+        private double mass;
+
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        public string DensityString
+        {
+            get => densityString; set
+            {
+                Set(ref densityString, value);
+                RaisePropertyChanged(nameof(Mass));
+            }
+        }
+        public double Density { get => DensityString.GetDouble(); }
+
+        public string DiameterString
+        {
+            get => diameterString; set
+            {
+                Set(ref diameterString, value);
+                RaisePropertyChanged(nameof(Mass));
+            }
+        }
+        public double Diameter { get => DiameterString.GetDouble(); }
+
+        public string LengthString
+        {
+            get => lengthString; set
+            {
+                Set(ref lengthString, value);
+                RaisePropertyChanged(nameof(Mass));
+            }
+        }
+
+        public double Length { get => LengthString.GetDouble(); }
+
+        public double Mass { get => Math.PI * Density * (Diameter * Diameter / 4) * (Length / 1000) / 1000000; set => Set(ref mass, value); }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+        internal void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+        #endregion
+    }
+}
---
+++ MassCalc/Util.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassCalc
+{
+    public static class Util
+    {
+        public enum GetNumberOption { Any, OnlyPositive }
+
+        /// <summary>
+        /// Получает число из строки
+        /// </summary>
+        /// <param name="stringNumber">Строка для получения</param>
+        /// <param name="defaultValue">Значение по умолчанию</param>
+        /// <param name="numberOption">Возвращаемое значение: только положительное или любое</param>
+        /// <returns>Значение Double, при неудаче возвращает значение по умолчанию</returns>
+        public static double GetDouble(this string stringNumber, double defaultValue = 0, GetNumberOption numberOption = GetNumberOption.OnlyPositive)
+        {
+            NumberFormatInfo numberFomat = new() { NumberDecimalSeparator = "," };
+            if (Double.TryParse(stringNumber, NumberStyles.Any, numberFomat, out double result))
+            {
+                if (numberOption == GetNumberOption.OnlyPositive && result > 0)
+                {
+                    return result;
+                }
+                else
+                {
+                    return defaultValue;
+                }
+            }
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// Получает число из строки
+        /// </summary>
+        /// <param name="stringNumber">Строка для получения</param>
+        /// <param name="defaultValue">Значение по умолчанию</param>
+        /// <param name="numberOption">Возвращаемое значение: только положительное или любое</param>
+        /// <returns>Значение Int32, при неудаче возвращает значение по умолчанию</returns>
+        public static int GetInt(this string stringNumber, int defaultValue = 0, GetNumberOption numberOption = GetNumberOption.OnlyPositive)
+        {
+            NumberFormatInfo numberFomat = new() { NumberDecimalSeparator = "," };
+            if (Int32.TryParse(stringNumber, NumberStyles.Any, numberFomat, out int result))
+            {
+                if (numberOption == GetNumberOption.OnlyPositive && result > 0)
+                {
+                    return result;
+                }
+                else
+                {
+                    return defaultValue;
+                }
+            }
+            return defaultValue;
+        }
+
+    }
+}
