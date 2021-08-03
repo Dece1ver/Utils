3091edde22e03650713a6bb26c1a001831048c52
Vladimir Gromov <v.dece1verr@hotmail.com>
2021-08-04 01:31:33 +0300

    начал шаблон wpf mvvm

---
+++ MVVM.Template/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,73 @@
+﻿using MVVM.Template.Infrastructure.Commands;
+using MVVM.Template.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MVVM.Template.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Заголовок окна";
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        private string _Status = string.Empty;
+        /// <summary>
+        /// Статус
+        /// </summary>
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+
+        private double _Progress;
+        /// <summary>
+        /// Значение прогрессбара
+        /// </summary>
+        public double Progress
+        {
+            get => _Progress;
+            set => Set(ref _Progress, value);
+        }
+
+        private double _ProgressMaxValue;
+        /// <summary>
+        /// Максимальное значение прогрессбара
+        /// </summary>
+        public double ProgressMaxValue
+        {
+            get => _ProgressMaxValue;
+            set => Set(ref _ProgressMaxValue, value);
+        }
+
+        #region Команды
+
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        private bool CanCloseApplicationCommandExecute(object p) => true; 
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+        }
+    }
+}
