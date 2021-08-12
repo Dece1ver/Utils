8904bc25f78e25b568d51cfc405e374f4782b974
Vladimir Gromov <v.dece1verr@hotmail.com>
2021-08-12 10:55:06 +0300

    скриптик для получения списка инструмента из УП

---
+++ NCToolTable/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NCToolTable
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new();
+
+            if (fileDialog.ShowDialog().Value)
+            {
+                var tools = new List<string>();
+                var filePath = fileDialog.FileName;
+                var lines = File.ReadAllLines(filePath);
+                foreach (var line in lines)
+                {
+                    if (line.Contains("T") && line.Contains("("))
+                    {
+                        tools.Add("(" + line.Split("(")[1].Trim());
+                    }
+                }
+                if (tools.Count > 0)
+                {
+                    Clipboard.SetText(string.Join(Environment.NewLine, tools));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выбор файла отменен.");
+            }
+            Close();
+        }
+    }
+}
