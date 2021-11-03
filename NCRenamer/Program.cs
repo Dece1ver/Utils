efae09b019df53ff179dcfd921d0a3d05b514e66
Vladimir Gromov <v.dece1verr@hotmail.com>
2021-11-03 22:57:13 +0300

    чутка рефактора и описание

--- NCRenamer/Program.cs
+++ NCRenamer/Program.cs
@@ -1,18 +1,13 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Diagnostics;
+﻿using System;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Security.AccessControl;
 using System.Security.Principal;
-using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace NCRenamer
 {
     class Program
     {
-        private const string registryName = "ncRenamer";
+        private static bool IsElevated => new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
 
         [DllImport("kernel32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
@@ -39,77 +34,11 @@ namespace NCRenamer
             for (int i = 0; i < files.Length; i++)
             {
                 FileInfo file = new(files[i]);
-                string newName = Reader.GetNcName(file.FullName);
+                string newName = Util.GetNcName(file.FullName);
                 if (newName == string.Empty) break;
                 //if (File.Exists(Path.Combine(file.Directory.FullName, newName))) file.Delete();
                 file.Rename(newName);
             }
-        }
-
-        /// <summary>
-        /// Сохраняет иконку приложения рядом с собой
-        /// </summary>
-        private static void SaveIcon()
-        {
-            File.WriteAllBytes("NCRenamer.ico", Properties.Resources.rename);
-        }
-
-        private static void AddToContextManu()
-        {
-            SaveIcon();
-            RegistryKey key;
-            WindowsIdentity identify = WindowsIdentity.GetCurrent();
-            RegistrySecurity regSecurity = new RegistrySecurity();
-            RegistryAccessRule accessRule = new RegistryAccessRule(identify.User, RegistryRights.FullControl, AccessControlType.Allow);
-            regSecurity.SetAccessRule(accessRule);
-            Console.Write("Создание раздела реестра...");
-            try
-            {
-                key = Registry.ClassesRoot.CreateSubKey(@"*\\shell\\" + registryName, RegistryKeyPermissionCheck.ReadWriteSubTree, regSecurity);
-                Console.Write("Успешно\nДобавление программы в контекстное меню...");
-                key.SetValue("", "Переименовать УП");
-                Console.Write("Успешно\nДобавление иконки в контекстное меню...");
-                key.SetValue("Icon", Path.GetFullPath("NCRenamer.ico"));
-                Console.Write("Успешно\nДля продолжения нажмите любую клавишу...\n");
-
-                key = Registry.ClassesRoot.CreateSubKey(@"*\\shell\\" + registryName + "\\command", RegistryKeyPermissionCheck.ReadWriteSubTree, regSecurity);
-                //путь к программе для запуска
-                key.SetValue("", $"\"{Path.GetFullPath(Process.GetCurrentProcess().ProcessName)}.exe\" \"%1\""); ;
-            }
-            catch(UnauthorizedAccessException)
-            {
-                Console.WriteLine("\nНедостаточно прав для выполнения операции. Попробуте запустить от имени администратора.");
-            }
-            Console.ReadKey();
-
-        }
-
-        private static void RemoveFromContextManu()
-        {
-            RegistryKey key;
-            WindowsIdentity identify = WindowsIdentity.GetCurrent();
-            RegistrySecurity regSecurity = new RegistrySecurity();
-            RegistryAccessRule accessRule = new RegistryAccessRule(identify.User, RegistryRights.FullControl, AccessControlType.Allow);
-            regSecurity.SetAccessRule(accessRule);
-            key = Registry.ClassesRoot.OpenSubKey(@"*\\shell\\", true);
-            
-            try
-            {
-                key = Registry.ClassesRoot.OpenSubKey(@"*\\shell\\", true);
-                key.DeleteSubKeyTree(registryName);
-                Console.WriteLine("Удалено.\nДля продолжения нажмите любую клавишу...");
-            }
-            catch (UnauthorizedAccessException)
-            {
-
-                Console.WriteLine("\nНедостаточно прав для выполнения операции. Попробуте запустить от имени администратора.");
-            }
-            catch (ArgumentException)
-            {
-                Console.WriteLine("Программы нет в контекстном меню.\nДля продолжения нажмите любую клавишу...");
-            }
-            Console.ReadKey();
-
         }
 
         private static void RunConsole()
@@ -117,44 +46,51 @@ namespace NCRenamer
             // Запускаем консоль.
             if (AllocConsole())
             {
-                Console.Title = "Переименовыватель УП";
-                
+                Console.Title = $"Переименовыватель УП {(IsElevated ? string.Empty : "(Ограниченный режим)")}";
                 while (true)
                 {
                     Console.Clear();
-                    Console.WriteLine("Настройка программы:\n\n" +
-                        $"[1] Добавить программу в контекстное меню Windows\n" +
-                        $"[2] Убрать программу из контекстного меню Windows\n" +
-                        $"[3] {(!File.Exists("NCRenamer.ico") ? "Создать" : "Перезаписать")} иконку приложения.\n" +
+                    Console.WriteLine($"{(IsElevated ? string.Empty : "Программа запущена без прав Администратора, работа в ограниченном режиме\n\n")}" +
+                        $"[1] Добавить программу в контекстное меню Windows{(IsElevated ? string.Empty : " (недоступно в ограниченном режиме)")}\n" +
+                        $"[2] Убрать программу из контекстного меню Windows{(IsElevated ? string.Empty : " (недоступно в ограниченном режиме)")}\n" +
+                        $"[3] О программе\n" +
                         "\n" +
-                        "[0] Закрыть программу.\n");
+                        "[0] Закрыть программу\n");
                     Console.Write(">");
-                    // Считываем данные.
                     var output = Console.ReadKey().Key;
-                    if (output == ConsoleKey.D0 || output == ConsoleKey.NumPad0)
+                    if (output is ConsoleKey.D0 or ConsoleKey.NumPad0)
                     {
                         break;
                     }
-                    if (output == ConsoleKey.D1 || output == ConsoleKey.NumPad1)
+                    if (output is ConsoleKey.D1 or ConsoleKey.NumPad1 && IsElevated)
                     {
                         Console.Clear();
-                        AddToContextManu();
+                        Util.AddToContextManu();
                     }
-                    if (output == ConsoleKey.D2 || output == ConsoleKey.NumPad2)
+                    if (output is ConsoleKey.D2 or ConsoleKey.NumPad2 && IsElevated)
                     {
                         Console.Clear();
-                        RemoveFromContextManu();
+                        Util.RemoveFromContextManu();
                     }
-                    if (output == ConsoleKey.D3 || output == ConsoleKey.NumPad3)
+                    if (output is ConsoleKey.D3 or ConsoleKey.NumPad3)
                     {
-                        SaveIcon();
                         Console.Clear();
-                        Console.WriteLine("Иконка создана. Для продолжения нажмите любую клавишу...");
+                        Console.WriteLine("Программа переименовывает файлы управляющих программ по названию самой управляющей программы.\n\n" +
+                            "Поддерживаемые СЧПУ:\n" +
+                            "* Fanuc 0i       [ O0001(НАЗВАНИЕ) ]\n" +
+                            "* Fanuc 0i-*F    [ <НАЗВАНИЕ> ]\n" +
+                            "* Mazatrol Smart [ .PBG | .PBD ]\n" +
+                            "* Hiedenhain     [ BEGIN PGM НАЗВАНИЕ MM ]\n");
+                        Console.WriteLine("Использование программы подразумевается вызовом через контекстное меню по переменовываемым файлам. (ПКМ)\n" +
+                            "Работа программы проиходит без каких либо уведомлений.\n" +
+                            "Если программа определяет файл как УП, то происходит переименование.\n" +
+                            "Если файл уже существует он не перезаписывается, а создается копия с добавлением номера.");
+                        Console.WriteLine("\nДля продолжения нажмите любую клавишу...");
                         Console.ReadKey();
                     }
                 }
                 // Закрываем консоль.
-                FreeConsole();
+                _ = FreeConsole();
             }
         }
     }
--- NCRenamer/Util.cs
+++ NCRenamer/Util.cs
@@ -1,28 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.AccessControl;
+using System.Security.Principal;
 
 namespace NCRenamer
 {
-    public static class Reader
+    public static class Util
     {
-        public const string Nameless = "NONAME";
+        private const string registryName = "ncRenamer";
+        private const string Nameless = "NONAME";
 
         public static readonly string[] mazatrolExtensions = { ".pbg", ".pbd", ".eia" };
         public static readonly string[] heidenhainExtensions = { ".h" };
         public static readonly string[] sinumerikExtensions = { ".mpf", ".spf" };
         public static readonly string[] otherNcExtensions = { ".nc", ".tap" };
 
-        public static readonly string[] machineExtensions = otherNcExtensions
+        public static readonly string[] machineExtensions = Array.Empty<string>()
             .Concat(heidenhainExtensions)
             .Concat(mazatrolExtensions)
             .Concat(sinumerikExtensions)
             .ToArray();
 
-
+        #region Чтение УП
         /// <summary>
         /// Читает файл УП СЧПУ Fanuc в поисках названия УП.
         /// </summary>
@@ -76,7 +78,7 @@ namespace NCRenamer
                 // если файл не пустой и начинается с %
                 if (!string.IsNullOrWhiteSpace(File.ReadAllText(filePath)))
                 {
-                    name = File.ReadAllLines(filePath)[0].Replace("BEGIN PGM ", string.Empty).TrimStart('0').Trim() ; // берем название с 1 строки обрезая лишнее
+                    name = File.ReadAllLines(filePath)[0].Replace("BEGIN PGM ", string.Empty).TrimStart('0').Trim(); // берем название с 1 строки обрезая лишнее
                     name = name.Remove(name.Length - 3);
                 }
                 else
@@ -135,11 +137,11 @@ namespace NCRenamer
             {
                 return GetMazatrolSmartName(filePath);
             }
-            else if (sinumerikExtensions.Contains(Path.GetExtension(filePath)?.ToLower())) 
+            else if (sinumerikExtensions.Contains(Path.GetExtension(filePath)?.ToLower()))
             {
                 return string.Empty; // написать обработчик
             }
-            else if (heidenhainExtensions.Contains(Path.GetExtension(filePath)?.ToLower())) 
+            else if (heidenhainExtensions.Contains(Path.GetExtension(filePath)?.ToLower()))
             {
                 return GetHeidenhainName(filePath);
             }
@@ -148,7 +150,9 @@ namespace NCRenamer
                 return GetFanucName(filePath);
             }
         }
+        #endregion
 
+        #region Расширения
         /// <summary>
         /// Переименовывает файл
         /// </summary>
@@ -159,6 +163,7 @@ namespace NCRenamer
             string tempExtension = string.Empty;
             if (machineExtensions.Contains(file.Extension)) tempExtension = file.Extension;
             string newFilePath = Path.Combine(file.Directory.FullName, newName + tempExtension);
+            if (file.FullName + tempExtension == newFilePath) return;
             int i = 1;
             string originalFilePath = newFilePath;
             while (File.Exists(newFilePath))
@@ -168,5 +173,66 @@ namespace NCRenamer
             file.MoveTo(newFilePath);
             //Microsoft.VisualBasic.FileIO.FileSystem.RenameFile(fileInfo.Directory.FullName, newName);
         }
+        #endregion
+
+        #region CLI
+        /// <summary>
+        /// Сохраняет иконку приложения рядом с собой
+        /// </summary>
+        private static void SaveIcon()
+        {
+            File.WriteAllBytes("NCRenamer.ico", Properties.Resources.rename);
+        }
+
+        public static void AddToContextManu()
+        {
+            SaveIcon();
+            RegistryKey key;
+            WindowsIdentity identify = WindowsIdentity.GetCurrent();
+            RegistrySecurity regSecurity = new();
+            RegistryAccessRule accessRule = new(identify.User, RegistryRights.FullControl, AccessControlType.Allow);
+            regSecurity.SetAccessRule(accessRule);
+            try
+            {
+                key = Registry.ClassesRoot.CreateSubKey(@"*\\shell\\" + registryName, RegistryKeyPermissionCheck.ReadWriteSubTree, regSecurity);
+                key.SetValue("", "Переименовать УП");
+                key.SetValue("Icon", Path.GetFullPath("NCRenamer.ico"));
+                key = Registry.ClassesRoot.CreateSubKey(@"*\\shell\\" + registryName + "\\command", RegistryKeyPermissionCheck.ReadWriteSubTree, regSecurity);
+                key.SetValue("", $"\"{Path.GetFullPath(Process.GetCurrentProcess().ProcessName)}.exe\" \"%1\"");
+                Console.Write("Программа успешно добавлена в контекстное меню Windows\n\nДля продолжения нажмите любую клавишу...");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("\nНедостаточно прав для выполнения операции. Попробуте запустить от имени администратора.");
+            }
+            Console.ReadKey();
+        }
+
+        public static void RemoveFromContextManu()
+        {
+            RegistryKey key;
+            WindowsIdentity identify = WindowsIdentity.GetCurrent();
+            RegistrySecurity regSecurity = new();
+            RegistryAccessRule accessRule = new(identify.User, RegistryRights.FullControl, AccessControlType.Allow);
+            regSecurity.SetAccessRule(accessRule);
+
+            try
+            {
+                key = Registry.ClassesRoot.OpenSubKey(@"*\\shell\\", true);
+                key.DeleteSubKeyTree(registryName);
+                Console.Write("Программа успешно удалена из контекстного меню Windows\n\nДля продолжения нажмите любую клавишу...");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("\nНедостаточно прав для выполнения операции. Попробуте запустить от имени администратора.");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Программы и так нет в контекстном меню\n\nДля продолжения нажмите любую клавишу...");
+            }
+            Console.ReadKey();
+
+        }
+        #endregion
     }
 }
