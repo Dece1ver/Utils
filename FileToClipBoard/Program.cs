4c440b6277ac555c16efb6e3dd9808ce529fd050
Vladimir Gromov <v.dece1verr@hotmail.com>
2021-11-29 12:05:24 +0300

    поиск и добавление файла в буфер

---
+++ FileToClipBoard/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileToClipBoard
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                string fileExtension = args[0];
+                if(!fileExtension.StartsWith(".")) fileExtension = "." + fileExtension;
+                string targetDir = args[1];
+                string fileName = string.Empty;
+                DateTime dt = new DateTime(1990, 1, 1);
+                FileSystemInfo[] fileSystemInfo = new DirectoryInfo(targetDir).GetFileSystemInfos();
+                foreach (FileSystemInfo fileSI in fileSystemInfo)
+                {
+                    if (fileSI.Extension == fileExtension)
+                    {
+                        if (dt < Convert.ToDateTime(fileSI.CreationTime))
+                        {
+                            dt = Convert.ToDateTime(fileSI.CreationTime);
+                            fileName = fileSI.FullName;
+                        }
+                    }
+                }
+                if (!string.IsNullOrEmpty(fileName))
+                {
+                    System.Collections.Specialized.StringCollection replacementList = new System.Collections.Specialized.StringCollection();
+                    replacementList.Add(fileName);
+                    Clipboard.SetFileDropList(replacementList);
+                    Console.WriteLine($"Файл \"{Path.GetFileName(fileName)}\" скопирован в буфер обмена.");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Программа ищет в целевой папке последний созданный файл с указанным расширением и добавляет его в буфер обмена. \n" +
+                    "Необходимо запускать программу передав 2 аргумента:\n\n" +
+                    "\t* Расширение файла (txt);\n" +
+                    "\t* Путь к целевой папке.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
