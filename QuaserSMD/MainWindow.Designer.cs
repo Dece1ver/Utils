757b089c004863ba62632c590ba35c77ed18f155
dece1ver <v.dece1verr@hotmail.com>
2023-05-24 11:45:47 +0300

    Синхронизировалка программ Гаврилюку

---
+++ QuaserSMD/MainWindow.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace QuaserSMD
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Quaser Sync Mounted Data";
+            this.notifyIcon.Visible = true;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(0, 0);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Quaser Sync Mounted Data";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
---
+++ QuaserSMD/MainWindow.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuaserSMD
+{
+    public partial class MainWindow : Form
+    {
+        private const string BasePath = "C:\\ProgramData\\dece1ver\\QuaserSMD";
+        private static readonly string ConfigPath = Path.Combine(BasePath, "config.ini");
+        private int timeOut = 5;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            var paths = new string[3];
+            if (!File.Exists(ConfigPath))
+            {
+                if (!Directory.Exists(BasePath)) Directory.CreateDirectory(BasePath);
+                File.WriteAllText(ConfigPath, "!Конфигурация Quaser SMD. Следующие 2 строки должны быть путями синхронизации.");
+                Process.Start(ConfigPath);
+                ShowNotification("Требуется настройка", "Создан и открыт файл конфигурации, необходимо его заполнить и заново запустить программу.", ToolTipIcon.Warning);
+            }
+            else
+            {
+                paths = File.ReadAllLines(ConfigPath).Skip(1).ToArray();
+                if (paths.Length != 2 || !Directory.Exists(paths[0]) || !Directory.Exists(paths[1]))
+                {
+                    Process.Start(ConfigPath);
+                    ShowNotification("Требуется настройка", "Открыт файл конфигурации, необходимо его корректно заполнить и заново запустить программу.", ToolTipIcon.Warning);
+                }
+                else
+                {
+                    var count = 0;
+                    foreach (var file in Directory.EnumerateFiles(paths[0]))
+                    {
+                        var guessFile = Path.Combine(paths[1], Path.GetFileName(file));
+                        if (!File.Exists(guessFile))
+                        {
+                            File.Copy(file, guessFile);
+                            count++;
+                        }
+                    }
+                    foreach (var file in Directory.EnumerateFiles(paths[1]))
+                    {
+                        var guessFile = Path.Combine(paths[0], Path.GetFileName(file));
+                        if (!File.Exists(guessFile))
+                        {
+                            File.Copy(file, guessFile);
+                            count++;
+                        }
+                    }
+
+                    ShowNotification("Завершено.",
+                        count == 0 ? "Копирование не требуется." : $"Скопировано файлов: {count}.");
+                }
+            }
+            Thread.Sleep(timeOut * 1000);
+            Close();
+        }
+
+        private void ShowNotification(string title, string message, ToolTipIcon icon = ToolTipIcon.Info)
+        {
+            notifyIcon.BalloonTipIcon = icon;
+            notifyIcon.BalloonTipText = message;
+            notifyIcon.BalloonTipTitle = title;
+            notifyIcon.ShowBalloonTip(timeOut);
+        }
+    }
+}
